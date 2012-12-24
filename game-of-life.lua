70e51f14257b13494ee18a3a35b9086a92172f5e
Ryan O'Leary <rjoleary314@gmail.com>
2012-12-24 15:49:56 -0500

    Finished rungame function

--- game-of-life.lua
+++ game-of-life.lua
@@ -53,11 +53,34 @@ end
 local function rungame(map, options)
   printmap(map, 0)
   for g = 1, options.generations do
-    for x = 1, map.width do
-      for y = 1, map.width do
+    local newmap = {width = map.width, height = map.height}
+    for y = 1, map.height do
+      newmap[y] = ''
+      for x = 1, map.width do
+        local neighbours = 0
+        if map[y-1] then
+          if map[y-1]:sub(x-1, x-1) == '#' then neighbours = neighbours + 1 end
+          if map[y-1]:sub(x, x)     == '#' then neighbours = neighbours + 1 end
+          if map[y-1]:sub(x+1, x+1) == '#' then neighbours = neighbours + 1 end
+        end
+        if map[y]:sub(x-1, x-1) == '#' then neighbours = neighbours + 1 end
+        if map[y]:sub(x+1, x+1) == '#' then neighbours = neighbours + 1 end
+        if map[y+1] then
+          if map[y+1]:sub(x-1, x-1) == '#' then neighbours = neighbours + 1 end
+          if map[y+1]:sub(x, x)     == '#' then neighbours = neighbours + 1 end
+          if map[y+1]:sub(x+1, x+1) == '#' then neighbours = neighbours + 1 end
+        end
 
+        if map[y]:sub(x, x) == '#' and neighbours ~= 2 and neighbours ~= 3 then
+          newmap[y] = newmap[y] .. '-'
+        elseif map[y]:sub(x, x) == '-' and neighbours == 3 then
+          newmap[y] = newmap[y] .. '#'
+        else
+          newmap[y] = newmap[y] .. map[y]:sub(x, x)
+        end
       end
     end
+    map = newmap
     printmap(map, g)
   end
 end
